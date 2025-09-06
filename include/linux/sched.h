8884e6fe89930148ac214f31c498314c2a696670
Soul Spark <spark_soul@mail.ru>
2025-09-06 05:38:43 +0000

    feat: Upgrade SuSFS to version 1.5.7
    - Apply susfs_upgrade_to_157.patch
    - Update fs/namespace.c with SuSFS 1.5.7 mount improvements
    - Update include/linux/susfs.h with new SuSFS interfaces
    - Fix duplicate susfs_last_fake_mnt_id field in sched.h
    - Enhance SuSFS mount hiding capabilities
    - Improve SuSFS kernel integration stability
    
    SuSFS version: 1.5.7

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1518,12 +1518,10 @@ struct task_struct {
 	 * New fields for task_struct should be added above here, so that
 	 * they are included in the randomized portion of task_struct.
 	 */
-#if defined(CONFIG_KSU_SUSFS) && !defined(ANDROID_KABI_RESERVE)
+	randomized_struct_fields_end
 	u64 susfs_task_state;
 #endif
-#if defined(CONFIG_KSU_SUSFS) && !defined(ANDROID_KABI_RESERVE)
-	u64 susfs_last_fake_mnt_id;
-#endif
+	randomized_struct_fields_end
 	randomized_struct_fields_end
 
 	/* CPU-specific state of this task: */
