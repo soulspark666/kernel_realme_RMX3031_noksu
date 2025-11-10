d942a86905487f9c6e8fd2e86ba7312ba33c764d
soulspark666 <spark_soul@mail.ru>
2025-11-10 12:43:40 +0000

    feat: Upgrade SuSFS to version 1.5.11 (4.19 no-kprobe)
    - Enhanced SUSFS mount checks to include umounted process validation.
    - Updated mount ID and group ID definitions for KSU.
    - Changed version from v1.5.9 to v1.5.11 in susfs.h.

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -111,7 +111,7 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID) &&
+	if (unlikely(r->mnt_id >= DEFAULT_KSU_MNT_ID) &&
 		(susfs_hide_sus_mnts_for_all_procs || !susfs_is_current_ksu_domain()))
 		return 0;
 #endif
@@ -153,7 +153,7 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID) &&
+	if (unlikely(r->mnt_id >= DEFAULT_KSU_MNT_ID) &&
 		(susfs_hide_sus_mnts_for_all_procs || !susfs_is_current_ksu_domain()))
 		return 0;
 #endif
@@ -223,7 +223,7 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID) &&
+	if (unlikely(r->mnt_id >= DEFAULT_KSU_MNT_ID) &&
 		(susfs_hide_sus_mnts_for_all_procs || !susfs_is_current_ksu_domain()))
 		return 0;
 #endif
